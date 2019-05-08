e7e16ab63466cdf58e8cd7088ef298608d056cd9
Bruno Thienel <bruno.thienel@googlemail.com>
2019-05-08 22:01:12 +0200

    first idea for settings websocket

--- python/api.py
+++ python/api.py
@@ -1,6 +1,10 @@
 from logging import Logger
+from .persistence.settings import Settings
+import responder
+import json
 
 log = Logger(__name__)
+responder_api = responder.API()
 
 
 class WebSocketMixin:
@@ -8,3 +12,40 @@ class WebSocketMixin:
 
     def send_via_websocket(self, msg):
         pass
+
+
+@responder_api.route('/settings', websocket=True)
+async def modify_settings(ws):
+    """
+    Websocket for settings. Takes json with elements:
+    cmd: string, parameters: list
+    :param ws:
+    :return:
+    """
+
+    # WARNING!
+    # I'm just goofing around. I'm not sure if this is a good way. If a way at all.
+    # Another idea is to use an adapter class that executes a method which is registered to a command.
+    # TODO: Needs some more brainstorming. Maybe a brain-tornado.
+
+    await ws.accept()
+    while True:
+        received_json = await ws.receive_json()
+        cmd = received_json['cmd']
+        params = received_json['paramters']
+        response = {'status': 500, 'response': ''}
+
+        try:
+            if cmd == 'get':
+                response['status'] = 200
+                response['response'] = Settings().get_value(*params)
+            elif cmd == 'set':
+                Settings().write_value(*params)
+                response['status'] = 200
+                response['response'] = ''
+        except Exception as e:
+            log.exception('Error in settings websocket.')
+            response['status'] = 500
+            response['response'] = str(e)
+
+        await ws.send_json(response)
