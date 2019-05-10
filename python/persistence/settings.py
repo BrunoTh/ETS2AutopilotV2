9a896fde9d37b7c7da36902257c66fc8bc9372ab
Bruno Thienel <bruno.thienel@googlemail.com>
2019-05-10 15:34:51 +0200

    first implementation of classes for dynamic settings structure (for UI)

--- python/persistence/settings.py
+++ python/persistence/settings.py
@@ -1,4 +1,5 @@
 from logging import Logger
+from abc import ABC, abstractmethod
 import json
 import pathlib
 
@@ -6,6 +7,103 @@ log = Logger(__name__)
 # Path object of python/ directory.
 BASE_PATH = pathlib.Path(__file__).parent
 DEFAULT_NAME = 'settings.json'
+
+
+"""
+Settings structure
+==================
+root
+  input_device = 
+    * keyboard
+      autopilot_button
+    * gamepad
+      device_id
+      autopilot_button
+      steering_axis
+    * wheel
+      device_id
+      autopilot_button
+      steering_axis
+  viewport
+    x1
+    x2
+    y1
+    y2
+  
+"""
+
+
+class HTMLWidget(ABC):
+    def __init__(self, settings_node):
+        self.settings_node = settings_node
+
+    @abstractmethod
+    def get_html_source(self) -> str:
+        """
+        This method returns html code of the widget.
+        """
+
+
+class SelectWidget(HTMLWidget):
+    def get_html_source(self):
+        return f'<input type="text" id="id_{self.settings_node.key}" value="{self.settings_node.value}" />'
+
+
+class SettingsNode:
+    possible_choices = []
+
+    def __init__(self, key='', value='', is_choice=False, widget=None):
+        self.children = []
+        self.is_root = False
+        self.is_choice = is_choice
+        self.widget = widget
+
+        self.key = key
+        self.value = value
+
+    def add_child(self, child):
+        if not isinstance(child, SettingsNode):
+            raise TypeError(f'Object of type {type(child)} is not supported.')
+        self.children.append(child)
+
+    def choose(self, children):
+        self.children = children.copy()
+
+    def set_value_of_child(self, child_path: str, value):
+        self.get_child_in_tree(child_path).value = value
+
+    def get_value_of_child(self, child_path: str):
+        return self.get_child_in_tree(child_path).value
+
+    def get_child_in_tree(self, child_path: str):
+        next_path_elements = child_path.split('.', maxsplit=1)
+        if len(next_path_elements) == 1:
+            return self
+        else:
+            for child in self.children:
+                if child.key == next_path_elements[0]:
+                    return child.get_value_of_child(next_path_elements[1])
+
+    def get_sub_tree(self):
+        """
+        This method is used to generate a dict structure of this settings tree.
+        It returns either a dict if this node still has children or just self.value.
+        """
+        if not self.children:
+            return self.value
+
+        result = dict()
+        for child in self.children:
+            if child.is_choice:
+                result['value'] = child.key
+                result.update(child.get_sub_tree())
+            else:
+                result[child.key] = child.get_sub_tree()
+        return result
+
+    def render_element(self):
+        if self.widget:
+            return self.widget(self).get_html_source()
 
 
 # TODO: singleton?
