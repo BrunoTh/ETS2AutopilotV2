fa2c26f5ee56bb3822d78f05e57a46e285341754
Bruno Thienel <bruno.thienel@googlemail.com>
2019-05-28 16:36:48 +0200

    added SettingsNode vjoy_device to VjoyController

--- python/controller.py
+++ python/controller.py
@@ -1,6 +1,7 @@
 from logging import Logger
 from abc import ABC, abstractmethod
 from .chain import ChainElement
+from .settingstree import SettingsNode
 
 log = Logger(__name__)
 
@@ -18,9 +19,10 @@ class VjoyController(ControllerInstance):
     """
     ControllerInstance for VJoy.
     """
-    def __init__(self, vjoy_device: int):
+    def __init__(self):
         super().__init__()
-        self.vjoy_device = vjoy_device
+
+        self.vjoy_device = SettingsNode(key='vjoy_device', value='0')
 
     def process(self, angle):
         # Write angle to vjoy.
