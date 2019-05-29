1bcf2e0794f2c704152b7807f57b5d3fba571860
Bruno Thienel <bruno.thienel@googlemail.com>
2019-05-29 12:17:44 +0200

    using platform specific chain, logging

--- python/autopilot.py
+++ python/autopilot.py
@@ -1,7 +1,17 @@
 import platform
 from chain.builtin import ProcessingChain
 from settingstree import Settings
+import logging
+
+log = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
 
 
 if __name__ == '__main__':
-    ProcessingChain.get_platform_specific_chain().run()
+    log.debug(f'Running on {platform.system()}')
+    try:
+        settings = Settings()
+        platform_specific_chain = ProcessingChain.get_platform_specific_chain()
+        platform_specific_chain(settings).run()
+    except (AttributeError, TypeError):
+        log.exception('Sorry! Your platform is not supported.')
