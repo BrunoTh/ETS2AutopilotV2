8af2509ac0cce3c7fc2233d4ff16daf874f5aadb
Bruno Thienel <bruno.thienel@googlemail.com>
2019-06-05 10:54:10 +0200

    changed ids of html elements

--- python/settingstree/widgets.py
+++ python/settingstree/widgets.py
@@ -2,22 +2,31 @@ from abc import ABC, abstractmethod
 
 
 class HTMLWidget(ABC):
-    @staticmethod
+    # TODO: Don't use staticmethod or classmethod.
+    @classmethod
     @abstractmethod
-    def get_html_source(settings_node) -> str:
+    def get_html_source(cls, settings_node) -> str:
         """
         This method returns html code of the widget.
         :type settings_node: SettingsNode
         """
+
+    @classmethod
+    def get_html_id(cls, settings_node) -> str:
+        """
+        This method returns the id of settings_node for html objects.
+        :param settings_node: SettingsNode
+        """
+        return settings_node.fqid
 
 
 class TextWidget(HTMLWidget):
     """
     Renders text input.
     """
-    @staticmethod
-    def get_html_source(settings_node) -> str:
-        return f'<input type="text" id="id_{settings_node.key}" value="{settings_node.value}" />'
+    @classmethod
+    def get_html_source(cls, settings_node) -> str:
+        return f'<input type="text" id="id_{cls.get_html_id(settings_node)}" value="{settings_node.value}" />'
 
 
 class SelectWidget(HTMLWidget):
@@ -25,8 +34,8 @@ class SelectWidget(HTMLWidget):
     Renders select tag.
     """
     @staticmethod
-    def get_html_source(settings_node) -> str:
-        html = f'<select id="id_{settings_node.key}">\n'
+    def get_html_source(cls, settings_node) -> str:
+        html = f'<select id="id_{cls.get_html_id(settings_node)}">\n'
 
         for child in settings_node.children:
             if child.is_choice and child.widget:
@@ -41,18 +50,18 @@ class OptionWidget(HTMLWidget):
     """
     Renders option tag.
     """
-    @staticmethod
-    def get_html_source(settings_node) -> str:
-        return f'<option id="id_{settings_node.key}">{settings_node.key}</option>'
+    @classmethod
+    def get_html_source(cls, settings_node) -> str:
+        return f'<option id="id_{cls.get_html_id(settings_node)}">{settings_node.key}</option>'
 
 
 class SubtreeWidget(HTMLWidget):
     """
     Renders child elements in div.
     """
-    @staticmethod
-    def get_html_source(settings_node) -> str:
-        html_code = f'<div id="{settings_node.key}">\n'
+    @classmethod
+    def get_html_source(cls, settings_node) -> str:
+        html_code = f'<div id="div_{cls.get_html_id(settings_node)}">\n'
         html_code += f'<h2>{settings_node.verbose_name if settings_node.verbose_name else settings_node.key}</h2>\n'
 
         for child in settings_node.children:
