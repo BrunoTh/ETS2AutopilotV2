b020ae25d1494f755e172f1af2e7731836b09a11
Bruno Thienel <bruno.thienel@googlemail.com>
2019-12-21 18:54:28 +0100

    implemented vjoy controller instance

--- python/chain/controller.py
+++ python/chain/controller.py
@@ -11,7 +11,7 @@ class ControllerInstance(ChainElement):
     def process(self, angle, *args, **kwargs):
         """
         This method takes a steering angle and passes it to a controller device.
-        :param angle: Steering angle
+        :param angle: Steering angle (-1 <= angle <= 1)
         """
 
 
@@ -24,6 +24,17 @@ class VjoyController(ControllerInstance):
 
         self.vjoy_device = SettingsNode(key='vjoy_device', value='0', widget=NodeInput, verbose_name='vJoy Device ID')
 
+    def import_dependencies(self):
+        self._import_helper('pyvjoy', 'pyvjoy')
+
     def process(self, angle, *args, **kwargs):
         # Write angle to vjoy.
-        return ProcessingResult()
+        vjoy_controller = self._imported_dependencies['pyvjoy'].VJoyDevice(int(self.vjoy_device.value))
+        vjoy_controller.reset()
+
+        vjoy_angle = (angle + 1) * 32768 / 2
+
+        vjoy_controller.set_axis(self._imported_dependencies['pyvjoy'].HID_USAGE_X, vjoy_angle)
+
+        # Send angle to UI
+        return ProcessingResult(data_to_send={'angle': angle})
